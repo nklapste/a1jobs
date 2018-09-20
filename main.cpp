6d974d5b9047488cf98896a9a9f8449034ed55ec
Nathan Klapstein <nklapste@ualberta.ca>
2018-09-19 21:37:05 -0400

    refactoring
    functioning

--- main.cpp
+++ main.cpp
@@ -14,6 +14,20 @@
 static const int MAXJOBS = 32;
 
 
+void suspend_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, std::vector<std::string> &tokens);
+
+void resume_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, std::vector<std::string> &tokens);
+
+void exit(const std::vector<std::tuple<int, pid_t, std::string>> &jobs);
+
+void terminate_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, std::vector<std::string> &tokens);
+
+void list_jobs(const std::vector<std::tuple<int, pid_t, std::string>> &jobs);
+
+void invalid_command(std::vector<std::string> &tokens);
+
+void run_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, int job_idx, std::vector<std::string> &tokens);
+
 int main() {
     rlimit rilimit{};
     rilimit.rlim_cur = 600;
@@ -48,14 +62,33 @@ int main() {
         if (tokens.empty()) {
             std::cout << "ERROR: Missing command" << std::endl;
         } else if (tokens.at(0) == "list") {
-            for(std::tuple<int, pid_t, std::string> job: jobs){
-                printf("%d: (pid=%6d, cmd= %s)\n",std::get<0>(job), std::get<1>(job), std::get<2>(job).c_str());
-            }
+            list_jobs(jobs);
         } else if (tokens.at(0) == "run") {
-            if (job_idx < MAXJOBS) {
+            run_job(jobs, job_idx, tokens);
+        } else if (tokens.at(0) == "suspend") {
+            suspend_job(jobs, tokens);
+        } else if (tokens.at(0) == "resume") {
+            resume_job(jobs, tokens);
+        } else if (tokens.at(0) == "terminate") {
+            terminate_job(jobs, tokens);
+        } else if (tokens.at(0) == "exit") {
+            exit(jobs);
+            break;
+        } else if (tokens.at(0) == "quit") {
+            printf("WARNING: Exiting a1jobs without terminating head processes\n");
+            break;
+        } else {
+            invalid_command(tokens);
+        }
+    }
+
+    return 0;
+}
+
+void run_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, int job_idx, std::vector<std::string> &tokens) {
+    if (job_idx < MAXJOBS) {
                 pid_t c_pid = fork();
                 errno = 0;
-
                 if (c_pid == 0) {
                     switch (tokens.size()) {
                         case 1:
@@ -79,8 +112,7 @@ int main() {
                             std::cout << "ERROR: Too many args for run" << std::endl;
                             break;
                     }
-
-                    return 0;
+                    exit(0);
                 }
                 if (errno) {
                     std::cout << "ERROR: Running command" << std::endl;
@@ -88,8 +120,8 @@ int main() {
                 } else {
                     // concat the cmd vector into a single string
                     std::ostringstream cmd_str;
-                    std::copy(tokens.begin() + 1, tokens.end() - 1,
-                              std::ostream_iterator<std::string>(cmd_str, " "));
+                    copy(tokens.begin() + 1, tokens.end() - 1,
+                              std::ostream_iterator<std::__cxx11::string>(cmd_str, " "));
                     cmd_str << tokens.back();
                     std::cout << cmd_str.str() << std::endl;
                     // append the job to the jobs list
@@ -100,54 +132,61 @@ int main() {
             } else {
                 std::cout << "ERROR: Too many jobs already initiated" << std::endl;
             }
-        } else if (tokens.at(0) == "suspend") {
-            int jobNo = std::stoi(tokens.at(1), nullptr, 10);
-            auto it = std::find_if(jobs.begin(), jobs.end(), [&jobNo](const std::tuple<int, pid_t, std::string>& job) {return std::get<1>(job) == jobNo;});
-            if (it != jobs.end()) {
-                printf("found job: %d suspending\n", jobNo);
-                kill(jobNo, SIGSTOP);
-            } else {
-                printf("ERROR: failed to find job: %d  not suspending\n", jobNo);
+}
+
+void invalid_command(std::vector<std::string> &tokens) {
+    std::ostringstream cmd_str;
+    copy(tokens.begin() + 1, tokens.end() - 1,
+                      std::ostream_iterator<std::__cxx11::string>(cmd_str, " "));
+    cmd_str << tokens.back();
+    // append the job to the jobs list
+    printf("ERROR: Invalid command: %s", cmd_str.str().c_str());
+}
+
+void list_jobs(const std::vector<std::tuple<int, pid_t, std::string>> &jobs) {
+    for(std::tuple<int, pid_t, std::__cxx11::string> job: jobs){
+                printf("%d: (pid=%6d, cmd= %s)\n", std::get<0>(job), std::get<1>(job), std::get<2>(job).c_str());
             }
-        } else if (tokens.at(0) == "resume") {
-            int jobNo = std::stoi(tokens.at(1), nullptr, 10);
-            auto it = std::find_if(jobs.begin(), jobs.end(), [&jobNo](const std::tuple<int, pid_t, std::string>& job) {return std::get<1>(job) == jobNo;});
-            if (it != jobs.end()) {
-                printf("found job: %d resuming\n", jobNo);
-                kill(jobNo, SIGCONT);
-            } else {
-                printf("ERROR: failed to find job: %d  not resuming\n", jobNo);
-            }
-        } else if (tokens.at(0) == "terminate") {
-            pid_t jobNo = std::stoi(tokens.at(1), nullptr, 10);
-            std::cout << jobNo << std::endl;
-            auto it = std::find_if(jobs.begin(), jobs.end(), [&jobNo](const std::tuple<int, pid_t, std::string>& job) {return std::get<1>(job) == jobNo;});
-            if (it != jobs.end()) {
+}
+
+void terminate_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, std::vector<std::string> &tokens) {
+    pid_t jobNo = stoi(tokens.at(1), nullptr, 10);
+    std::cout << jobNo << std::endl;
+    auto it = find_if(jobs.begin(), jobs.end(), [&jobNo](const std::tuple<int, pid_t, std::__cxx11::string>& job) {return std::get<1>(job) == jobNo;});
+    if (it != jobs.end()) {
                 printf("found job: %d terminating\n", jobNo);
                 jobs.erase(it);
                 kill(jobNo, SIGKILL);
             } else {
                 printf("ERROR: failed to find job: %d  not terminating\n", jobNo);
             }
+}
 
-        } else if (tokens.at(0) == "exit") {
-            for(std::tuple<int, pid_t, std::string> job: jobs){
+void exit(const std::vector<std::tuple<int, pid_t, std::string>> &jobs) {
+    for(std::tuple<int, pid_t, std::__cxx11::string> job: jobs){
                 printf("terminating job: %d\n", std::get<1>(job));
                 kill(std::get<1>(job), SIGKILL);
             }
-            break;
-        } else if (tokens.at(0) == "quit") {
-            printf("WARNING: Exiting a1jobs without terminating head processes\n");
-            break;
-        } else {
-            std::ostringstream cmd_str;
-            std::copy(tokens.begin() + 1, tokens.end() - 1,
-                      std::ostream_iterator<std::string>(cmd_str, " "));
-            cmd_str << tokens.back();
-            // append the job to the jobs list
-            printf("ERROR: Invalid command: %s", cmd_str.str().c_str());
-        }
-    }
+}
 
-    return 0;
+void resume_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, std::vector<std::string> &tokens) {
+    int jobNo = stoi(tokens.at(1), nullptr, 10);
+    auto it = find_if(jobs.begin(), jobs.end(), [&jobNo](const std::tuple<int, pid_t, std::__cxx11::string>& job) {return std::get<1>(job) == jobNo;});
+    if (it != jobs.end()) {
+                printf("found job: %d resuming\n", jobNo);
+                kill(jobNo, SIGCONT);
+            } else {
+                printf("ERROR: failed to find job: %d  not resuming\n", jobNo);
+            }
+}
+
+void suspend_job(std::vector<std::tuple<int, pid_t, std::string>> &jobs, std::vector<std::string> &tokens) {
+    int jobNo = stoi(tokens.at(1), nullptr, 10);
+    auto it = find_if(jobs.begin(), jobs.end(), [&jobNo](const std::tuple<int, pid_t, std::__cxx11::string>& job) {return std::get<1>(job) == jobNo;});
+    if (it != jobs.end()) {
+                printf("found job: %d suspending\n", jobNo);
+                kill(jobNo, SIGSTOP);
+            } else {
+                printf("ERROR: failed to find job: %d  not suspending\n", jobNo);
+            }
 }
